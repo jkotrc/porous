fc7a38576403f751369bb7a3e42686da3f41f30a
jkotrc <john.kotrc@gmail.com>
2020-12-08 13:20:48 +0100

    working prototype. No error checking

--- test-view.py
+++ test-view.py
@@ -3,9 +3,9 @@ import h5py as h5
 import numpy as np
 from sys import argv
 
-f=h5.File("out_test.h5",'r')
+f=h5.File(argv[1],'r')
 
-img = np.array(f['images'][int(argv[1])])
+img = np.array(f['images'][int(argv[2])])
 
 plt.matshow(img)
 plt.show()
--- utility.cpp
+++
@@ -1,9 +0,0 @@
-#include "utility.h"
-#include <stdlib.h>
-#include <cstring>
-
-#define STARTING_SIZE 10*sizeof(double)
-using namespace porous;
-using namespace std;
-
-
