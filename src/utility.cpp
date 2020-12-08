7eb8d572e3dacd0171d9e45f16e2730e8f71c049
jkotrc <john.kotrc@gmail.com>
2020-12-08 13:48:39 +0100

    added README

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,7 +1,1 @@
 #include "utility.h"
-#include <stdlib.h>
-#include <cstring>
-
-#define STARTING_SIZE 10*sizeof(double)
-using namespace porous;
-using namespace std;
--- test-view.py
+++ test-view.py
@@ -1,11 +1,18 @@
+'''
+tiny script for plotting the image at some index for the output HD5 file
+'''
 import matplotlib.pyplot as plt
 import h5py as h5
 import numpy as np
-from sys import argv
+import sys
 
-f=h5.File(argv[1],'r')
+if len(sys.argv) != 3:
+	print("USAGE: test-view.py <filename> <data index>")
+	exit()
 
-img = np.array(f['images'][int(argv[2])])
+f=h5.File(sys.argv[1],'r')
+
+img = np.array(f['images'][int(sys.argv[2])])
 
 plt.matshow(img)
 plt.show()
